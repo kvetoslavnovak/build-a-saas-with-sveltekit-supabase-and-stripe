85c84d37f17f0141b2769b093e1939ca37487e88
kvetoslavnovak <kvetoslavnovak@users.noreply.github.com>
2022-08-04 14:18:37 +0200

    Delete test.svelte
    Deleting test file

--- src/routes/test.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    // import { user } from '$lib/authStore';
-    import { session } from '$app/stores';
-    // console.log("user from store: ", $user);
-    console.log("Test page, user from session: ", $session.user);
-</script>
-<!-- {JSON.stringify($user)} -->
-{JSON.stringify($session.user)}
-Test
